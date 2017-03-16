e8c7b3f04e35786881eb1f106a0b30fd04eaec3e
Karim Naaji <karim.naaji@gmail.com>
2017-03-16 10:41:52 -0400

    Add default scene with initial code to perform HTTP requests to mapzen services

---
+++ Assets/Mapzen.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.InteropServices;
+
+public class Mapzen : MonoBehaviour {
+
+	[DllImport("UnityNativePlugin", EntryPoint = "UnityNativePlugin")]
+	public static extern void UnityNativePlugin();
+
+	delegate void HTTPRequestCallback(string error, string response);
+	private UnityWebRequest request;
+	private HTTPRequestCallback callback;
+
+	public int tilex = 19294;
+	public int tiley = 24642;
+	public int tilez = 16;
+
+	void Start () {
+		//UnityNativePlugin ();
+
+		// Construct the HTTP request
+		{
+			string url = "https://tile.mapzen.com/mapzen/vector/v1/all/"
+				+ this.tilex.ToString () + "/"
+				+ this.tiley.ToString () + "/"
+				+ this.tilez.ToString () + "/"
+				+ ".json?api_key=vector-tiles-tyHL4AY";
+
+			this.callback = delegate (string error, string response) {
+				Debug.Log ("Error: " + error);
+				Debug.Log ("Response: " + response);
+
+				// Adding a tile object to the scene
+				GameObject tilePrefab = Resources.Load ("Tile") as GameObject;
+
+				TileData data = tilePrefab.GetComponent<TileData>();
+				data.geoJSONData = response;
+
+				// Instantiate a prefab running the script TileData.Start()
+				Instantiate (tilePrefab);
+			};
+			this.request = UnityWebRequest.Get (url);
+		}
+
+		// Starts the HTTP request
+		StartCoroutine (this.DoHTTPRequest ());
+	}
+
+	// Runs an HTTP request
+	IEnumerator DoHTTPRequest() {
+		yield return this.request.Send ();
+
+		string data = System.Text.Encoding.Default.GetString (this.request.downloadHandler.data);
+		this.callback (this.request.error, data);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/TileData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour {
+
+	public string geoJSONData;
+
+	// Use this for initialization
+	void Start () {
+		if (geoJSONData.Length == 0) {
+			Debug.Log ("Empty geoJSON data");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
