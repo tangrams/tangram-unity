509121a2ad774513ade0a1d307a073ad0a71b45c
Matt Blair <blair1618@gmail.com>
2017-03-27 11:47:12 -0400

    Add core vector tile functionality (#9)
    - Create 'Mapzen' folder and namespace
    - Add Mapzen.Geo class geographic math
    - Add Mapzen.LngLat, Mapzen.MercatorMeters, and Mapzen.TileAddress structs
    - Create 'Mapzen.VectorData' folder and namespace
    - Add Mapzen.VectorData.Feature class to represent tile feature data
    - Add Mapzen.VectorData.FeatureCollection class to represent a named list of features
    - Add Mapzen.VectorData.GeoJSON class for transforming GeoJSON text into tile features
    - Add Mapzen.VectorData.Geometry class and Mapzen.VectorData.GeometryType enum to represent tile feature geometry
    - Add Mapzen.VectorData.Point struct to represent local tile positions
    - Rename Mapzen class to MapzenMap and clean up
    - Add MapTile MonoBehavior to attach tile data to a GameObject

---
+++ Assets/Mapzen/MercatorMeters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mapzen
+{
+	public class MercatorMeters
+	{
+		public MercatorMeters(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public double x;
+		public double y;
+
+		public LngLat ToLngLat()
+		{
+			return Geo.Unproject(this);
+		}
+
+	}
+}
---
+++ Assets/Mapzen/VectorData/FeatureCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Mapzen.VectorData
+{
+	public class FeatureCollection
+	{
+		public FeatureCollection(string name = "")
+		{
+			this.name = name;
+			this.features = new List<Feature>();
+		}
+
+		public string name;
+		public List<Feature> features;
+	}
+}
