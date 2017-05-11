5dddf6af646860de62e0d73adcf9d78538a4e364
Karim Naaji <karim.naaji@gmail.com>
2017-05-11 12:46:32 -0400

    Add MeshData and Builder class

--- Assets/MapTile.cs
+++ Assets/MapTile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mapzen.VectorData;
+using SimpleJSON;
 using LibTessDotNet;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -9,121 +10,61 @@ public class MapTile : MonoBehaviour
 {
 	public List<FeatureCollection> Layers;
 
-    private Dictionary<String, Color> layerColors = new Dictionary<String, Color> {
-        { "water", Color.blue },
-        { "earth", Color.green },
-        { "roads", Color.gray },
-        { "buildings", Color.black },
-    };
+        private Dictionary<String, Color> layerColors = new Dictionary<String, Color> {
+                { "water", Color.blue },
+                { "earth", Color.green },
+                { "roads", Color.gray },
+                { "buildings", Color.black },
+        };
 
 	public void BuildMesh(double tileScale)
 	{
-        var mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+		float inverseTileScale = 1.0f / (float)tileScale;
+		var meshData = new MeshData();
 
-        var indices = new List<int>();
-        var vertices = new List<Vector3>();
-        var colors = new List<Color>();
-
-        foreach (var layer in Layers)
-        {
-            Color color = layerColors[layer.name];
-
-            foreach (var feature in layer.features)
-            {
-                // TODO: use extrusion scale and minHeight as options
-                float height = 0.0f;
-                float minHeight = 0.0f;
-
-                // Extract height
-                SimpleJSON.JSONNode heightNode;
-                if (feature.TryGetProperty("height", out heightNode))
+                foreach (var layer in Layers)
                 {
-                    height = heightNode * (1.0f / (float)tileScale);
-                }
+                        Color color = layerColors[layer.name];
 
-                var tess = new Tess();
-
-                var geometry = feature.geometry;
-
-                if (geometry.type == GeometryType.Polygon)
-                {
-                    int pointIndex = 0;
-                    foreach (var ringSize in geometry.rings)
-                    {
-                        var contour = new ContourVertex[ringSize];
-
-                        for (int i = pointIndex, contourIndex = 0; i < pointIndex + ringSize; ++i, ++contourIndex)
+                        foreach (var feature in layer.features)
                         {
-                            var point = geometry.points[i];
-                            contour[contourIndex].Position = new Vec3 { X = point.x, Y = height, Z = point.y };
+                                // TODO: use extrusion scale and minHeight as options
+                                float height = 0.0f;
+                                float minHeight = 0.0f;
+
+                                JSONNode heightNode;
+                                if (feature.TryGetProperty("height", out heightNode))
+                                {
+                                    height = heightNode * inverseTileScale;
+                                }
+
+                                if (feature.geometry.type == GeometryType.Polygon)
+                                {
+                                        var polygonMeshData = Builder.TesselatePolygon(feature.geometry, color, height);
+                                        meshData.Add(polygonMeshData);
+
+                                        if (height > 0.0f)
+                                        {
+                                                var extrusionMeshData = Builder.TesselatePolygonExtrusion(feature.geometry, color, minHeight, height);
+                                                meshData.Add(extrusionMeshData);
+                                        }
+                                }
                         }
-
-                        pointIndex += ringSize;
-                        tess.AddContour(contour, ContourOrientation.Original);
-                    }
                 }
-                tess.Tessellate(WindingRule.NonZero, ElementType.Polygons, 3);
 
-                // Build walls
-                if (height > 0.0f)
+                meshData.FlipIndices();
+
+                // Initialize Unity mesh
                 {
-                    int pointIndex = 0;
-                    int indexOffset = vertices.Count;
+                        var mesh = new Mesh ();
+                        GetComponent<MeshFilter> ().mesh = mesh;
 
-                    foreach (var ringSize in geometry.rings)
-                    {
-                        for (int i = pointIndex; i < pointIndex + ringSize - 1; i++)
-                        {
-                            var p0 = geometry.points[i];
-                            var p1 = geometry.points[i+1];
-
-                            vertices.Add(new Vector3(p0.x, height, p0.y));
-                            vertices.Add(new Vector3(p1.x, height, p1.y));
-                            vertices.Add(new Vector3(p0.x, minHeight, p0.y));
-                            vertices.Add(new Vector3(p1.x, minHeight, p1.y));
-
-                            for (int colorIndex = 0; colorIndex < 4; ++colorIndex) {
-                                colors.Add(color);
-                            }
-
-                            indices.Add(indexOffset + 2);
-                            indices.Add(indexOffset + 3);
-                            indices.Add(indexOffset + 1);
-                            indices.Add(indexOffset + 2);
-                            indices.Add(indexOffset + 1);
-                            indices.Add(indexOffset + 0);
-
-                            indexOffset += 4;
-                        }
-
-                        pointIndex += ringSize;
-                    }
+                        mesh.vertices = meshData.vertices.ToArray ();
+                        mesh.triangles = meshData.indices.ToArray ();
+                        mesh.colors = meshData.colors.ToArray ();
+                        mesh.RecalculateNormals ();
                 }
-
-				int offset = vertices.Count;
-
-                for (int i = 0; i < tess.ElementCount * 3; ++i)
-                {
-                    indices.Add(offset + tess.Elements[i]);
-                }
-
-                for (int i = 0; i < tess.VertexCount; ++i)
-                {
-                    var position = tess.Vertices[i].Position;
-                    vertices.Add(new Vector3(position.X, position.Y, position.Z));
-                    colors.Add(color);
-                }
-            }
-		}
-
-		indices.Reverse();
-
-		mesh.vertices = vertices.ToArray();
-		mesh.triangles = indices.ToArray();
-        mesh.colors = colors.ToArray();
-        mesh.RecalculateNormals();
-	}
+        }
 
 	public void Update()
 	{
---
+++ Assets/MeshData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData
+{
+	public List<int> indices = new List<int>();
+	public List<Vector3> vertices = new List<Vector3>();
+	public List<Color> colors = new List<Color>();
+
+	public void FlipIndices()
+	{
+		indices.Reverse();
+	}
+
+	public void Add(MeshData meshData)
+	{
+		int indexOffset = vertices.Count;
+
+		for (int i = 0; i < meshData.indices.Count; ++i) 
+		{
+			indices.Add(indexOffset + meshData.indices[i]);
+		}
+
+		vertices.AddRange(meshData.vertices);
+		colors.AddRange(meshData.colors);
+	}
+}
