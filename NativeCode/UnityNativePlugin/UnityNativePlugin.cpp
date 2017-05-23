658154ec5615f7e2963717efdda05241f5368f23
Karim Naaji <karim.naaji@gmail.com>
2017-05-23 18:03:09 -0400

    Add Unity native plugin wrapper for earcut

---
+++ NativeCode/UnityNativePlugin/UnityNativePlugin.cpp
@@ -0,0 +1,66 @@
+#include "UnityNativePlugin.hpp"
+#include "earcut.hpp"
+#include <iostream>
+#include <vector>
+
+extern "C" {
+    using Coord = double;
+    using N = unsigned int;
+    using Point = std::array<Coord, 2>;
+    using PolygonArray = std::vector<std::vector<Point>>;
+    using EarcutContext = std::vector<mapbox::Earcut<Coord, N>>;
+    
+    static EarcutContext earcuts;
+    
+    unsigned int CreateTesselationContext()
+    {
+        earcuts.emplace_back();
+        return (unsigned int)earcuts.size() - 1;
+    }
+    
+    void ReleaseTesselationContext(unsigned int context)
+    {
+        earcuts[context].indices.clear();
+        earcuts[context].vertices.clear();
+    }
+ 
+    void TesselatePolygon(unsigned int context, char* pointsBuffer, char* ringsBuffer,
+                          int nRings, unsigned int& nIndices, unsigned int& nVertices)
+    {
+        auto& earcut = earcuts[context];
+        PolygonArray polygon;
+        
+        double* points = reinterpret_cast<double*>(pointsBuffer);
+        unsigned int* rings = reinterpret_cast<unsigned int*>(ringsBuffer);
+
+        int point = 0;
+
+        for (int i = 0; i < nRings; ++i) {
+            int ring = rings[i];
+
+            polygon.emplace_back();
+
+            for (int j = 0; j < ring; ++j) {
+                polygon.back().push_back({points[point], points[point+1]});
+                point += 2;
+            }
+        }
+
+        earcut(polygon);
+
+        nIndices = (unsigned int)earcut.indices.size();
+        nVertices = (unsigned int)earcut.vertices.size();
+    }
+    
+    void GetIndices(unsigned int context, char* indices)
+    {
+        std::memcpy(indices, earcuts[context].indices.data(),
+                    earcuts[context].indices.size() * sizeof(N));
+    }
+    
+    void GetVertices(unsigned int context, char* vertices)
+    {
+        std::memcpy(vertices, earcuts[context].vertices.data(),
+                    earcuts[context].vertices.size() * sizeof(Point));
+    }
+}
