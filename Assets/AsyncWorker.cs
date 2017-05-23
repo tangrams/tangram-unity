85cd0d646275ad38921c995b506dc8e3196db159
Matt Blair <blair1618@gmail.com>
2017-05-23 19:06:47 -0400

    Convert indentation to tabs, add trailing newlines

--- Assets/AsyncWorker.cs
+++ Assets/AsyncWorker.cs
@@ -3,94 +3,94 @@ using System.Threading;
 using System.Collections.Generic;
 
 public class AsyncWorker {
-        public delegate void Task();
-        private Thread[] threads;
-        private bool stopped;
-        private Queue<Task> tasks;
+	public delegate void Task();
+	private Thread[] threads;
+	private bool stopped;
+	private Queue<Task> tasks;
 
-        public AsyncWorker(int nThreads)
-        {
-                tasks = new Queue<Task>();
-                threads = new Thread[nThreads];
-                stopped = false;
+	public AsyncWorker(int nThreads)
+	{
+		tasks = new Queue<Task>();
+		threads = new Thread[nThreads];
+		stopped = false;
 
-                for (int i = 0; i < threads.Length; ++i) 
-                {
-                        threads[i] = new Thread(new ThreadStart(this.ThreadMain));
-                        threads[i].Start();
-                }
-        }
+		for (int i = 0; i < threads.Length; ++i)
+		{
+			threads[i] = new Thread(new ThreadStart(this.ThreadMain));
+			threads[i].Start();
+		}
+	}
 
-        public void RunAsync(Task task)
-        {
-                if (stopped) 
-                {
-                        return;
-                }
+	public void RunAsync(Task task)
+	{
+		if (stopped)
+		{
+			return;
+		}
 
-                lock (this) 
-                {
-                        tasks.Enqueue(task);
+		lock (this)
+		{
+			tasks.Enqueue(task);
 
-                        // Awaken one waiting thread
-                        Monitor.Pulse(this);
-                }
-        }
+			// Awaken one waiting thread
+			Monitor.Pulse(this);
+		}
+	}
 
-        public int RemainingTasks()
-        {
-                int tasksSize;
+	public int RemainingTasks()
+	{
+		int tasksSize;
 
-                lock (this) 
-                {
-                        tasksSize = tasks.Count;
-                }
+		lock (this)
+		{
+			tasksSize = tasks.Count;
+		}
 
-                return tasksSize;
-        }
+		return tasksSize;
+	}
 
-        public void JoinAll()
-        {
-                stopped = true;
+	public void JoinAll()
+	{
+		stopped = true;
 
-                lock (this) 
-                {
-                        // Awaken all waiting threads
-                        Monitor.PulseAll(this);
-                }
+		lock (this)
+		{
+			// Awaken all waiting threads
+			Monitor.PulseAll(this);
+		}
 
-                for (int i = 0; i < threads.Length; ++i) 
-                {
-                        threads[i].Join();
-                }
-        }
+		for (int i = 0; i < threads.Length; ++i)
+		{
+			threads[i].Join();
+		}
+	}
 
-        private void ThreadMain()
-        {
-                while (!stopped)
-                {
-                        Task task = null;
+	private void ThreadMain()
+	{
+		while (!stopped)
+		{
+			Task task = null;
 
-                        lock (this) 
-                        {
-                                // If a thread calls Pulse when no other threads are waiting, the Pulse is lost,
-                                // make sure that this threads keep consuming tasks unless the queue is empty.
-                                if (tasks.Count == 0) 
-                                {
-                                        Monitor.Wait(this);
-                                }
+			lock (this)
+			{
+				// If a thread calls Pulse when no other threads are waiting, the Pulse is lost,
+				// make sure that this threads keep consuming tasks unless the queue is empty.
+				if (tasks.Count == 0)
+				{
+					Monitor.Wait(this);
+				}
 
-                                // This thread might be awaken from a JoinAll() call with an empty queue
-                                if (tasks.Count > 0) 
-                                {
-                                        task = tasks.Dequeue();
-                                }
-                        }
+				// This thread might be awaken from a JoinAll() call with an empty queue
+				if (tasks.Count > 0)
+				{
+					task = tasks.Dequeue();
+				}
+			}
 
-                        if (task != null) 
-                        {
-                                task.Invoke();
-                        }
-                }
-        }
+			if (task != null)
+			{
+				task.Invoke();
+			}
+		}
+	}
 }
