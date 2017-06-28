ed81570589d983b6d12a3e8911e47bfe5897bb5e
Matt Blair <blair1618@gmail.com>
2017-06-28 01:28:14 -0400

    Ensure gzipped MVT tiles are decompressed before parsing
    On Windows, UnityWebRequest does not automatically decompress responses with a 'Content-Encoding=gzip' header.
    This adds a check for gzipped content and manually decompresses it using System.IO.Compression.GZipStream.

--- Assets/Mapzen/VectorData/Formats/MvtTile.cs
+++ Assets/Mapzen/VectorData/Formats/MvtTile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 
 namespace Mapzen.VectorData.Formats
 {
@@ -12,7 +14,25 @@ namespace Mapzen.VectorData.Formats
         public MvtTile(TileAddress address, byte[] data)
         {
             this.address = address;
-            this.tile = Mvtcs.Tile.Parser.ParseFrom(data);
+
+            // Check the start of the data to see if it is a gzip file.
+            // The first two bytes in a gzip file are the magic numbers 31 and 139: http://www.gzip.org/zlib/rfc-gzip.html
+            if (data.Length >= 2 && (data[0] == 31 && data[1] == 139))
+            {
+                // We need to decompress the gzip file before parsing it.
+                using (MemoryStream inputMemStream = new MemoryStream(data))
+                {
+                    using (GZipStream gzStream = new GZipStream(inputMemStream, CompressionMode.Decompress))
+                    {
+                        this.tile = PbfTile.Parser.ParseFrom(gzStream);
+                    }
+                }
+            }
+            else
+            {
+                // We can parse the bytes directly.
+                this.tile = PbfTile.Parser.ParseFrom(data);
+            }
         }
 
         public override IEnumerable<FeatureCollection> FeatureCollections
