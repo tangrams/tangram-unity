ee5ce4a19ed223277fa0ba762f8032a0bb380684
Karim Naaji <karim.naaji@gmail.com>
2017-08-25 12:25:13 -0400

    Add LayerStyleEditor

---
+++ Assets/Editor/LayerStyleEditor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class LayerStyleEditor
+{
+    private PolylineBuilderEditor polylineBuilderEditor;
+    private PolygonBuilderEditor polygonBuilderEditor;
+
+    public LayerStyleEditor()
+    {
+        polygonBuilderEditor = new PolygonBuilderEditor();
+        polylineBuilderEditor = new PolylineBuilderEditor();
+    }
+}
