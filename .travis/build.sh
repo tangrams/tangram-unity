6b7b51fc723310e0843a52338657ff809f2f6db2
Karim Naaji <karim.naaji@gmail.com>
2017-09-12 12:46:49 -0400

    Add build matrix

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-    -batchmode \
-    -nographics \
-    -silent-crashes \
-    -logFile $(pwd)/unity.log \
-    -projectpath $(pwd) \
-    -buildOSXUniversalPlayer "$(pwd)/tangram-unity.app" \
-    -quit
+if [[ ${PLATFORM} == "osx" ]]; then
+    /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+        -batchmode \
+        -nographics \
+        -silent-crashes \
+        -logFile $(pwd)/unity.log \
+        -projectpath $(pwd) \
+        -buildOSXUniversalPlayer "$(pwd)/tangram-unity.app" \
+        -quit
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+    echo 'Logs from build'
+    cat $(pwd)/unity.log
+
+    cd NativePlugin
+    cmake . -Bbuild
+    make -c build/
+fi
+
+if [[ ${PLATFORM} == "linux" ]]; then
+    cd NativePlugin
+    cmake . -Bbuild -DARCH=x86
+    make -c build/
+fi
+
+if [[ ${PLATFORM} == "android" ]]; then
+    cd NativePlugin
+    ./gradlew earcut:assembleFullRelease
+fi
