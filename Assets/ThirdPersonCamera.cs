425b7ab0ccc79a083cb7b1a5f0cbe4c8e79a5461
Karim Naaji <karim.naaji@gmail.com>
2017-10-25 13:24:42 -0400

    Auto adjustment of camera distance

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -6,18 +6,31 @@ public class ThirdPersonCamera : MonoBehaviour {
 
 	public GameObject FollowedObject;
 	public float InputSensitivity = 100.0f;
+	public const float Distance = 5.0f;
 
 	float theta = 0.0f;
 	float phi = 0.0f;
 
-	float distance = 5.0f;
+	float adjustedDistance = Distance;
 
-	void Start () {
+	struct CameraDesc
+	{
+		public Matrix4x4 view;
+		public Matrix4x4 projection;
+		public float nearClipPlane;
+		public float fieldOfView;
+		public float aspect;
+		public Vector3 position;
+	}
+
+	void Start()
+	{
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
 	
-	void Update () {
+	void Update()
+	{
 		float mouseX = Input.GetAxis("Mouse X");
 		float mouseY = Input.GetAxis("Mouse Y");
 
@@ -29,48 +42,88 @@ public class ThirdPersonCamera : MonoBehaviour {
 
 		Quaternion rotation = Quaternion.Euler(theta, phi, 0.0f);
 		Vector3 dir = new Vector3(0.0f, 1.0f, 0.0f);
-		Vector3 pointOnSphere = rotation * dir * distance;
+		Vector3 pointOnSphere = rotation * dir * Distance;
+
+		CameraDesc cameraDesc = new CameraDesc();
+		cameraDesc.view = Camera.main.cameraToWorldMatrix;
+		cameraDesc.projection = Camera.main.projectionMatrix;
+		cameraDesc.nearClipPlane = Camera.main.nearClipPlane;
+		cameraDesc.fieldOfView = Camera.main.fieldOfView;
+		cameraDesc.aspect = Camera.main.aspect;
+		cameraDesc.position = FollowedObject.transform.position + pointOnSphere;
+		cameraDesc.view = Matrix4x4.LookAt(Camera.main.transform.position,
+			FollowedObject.transform.position,
+			Vector3.up);
+
+		var frustrumPoints = ExtractFrustrumNearPlanePoints(cameraDesc);
+		float distance = GetMinimumCollisionDistanceFromTarget(frustrumPoints);
+
+		if (distance != -1)
+		{
+			adjustedDistance = distance;
+		}
+		else
+		{
+			adjustedDistance = Distance;
+		}
+
+		pointOnSphere = rotation * dir * adjustedDistance;
 
 		transform.position = FollowedObject.transform.position + pointOnSphere;
 		transform.LookAt(FollowedObject.transform);
 	}
 
-	void OnDrawGizmos() {
-		var frustrumPoints = ExtractFrustrumNearPlanePoints();
-		for (int i = 0; i < 4; ++i) {
-			Gizmos.DrawSphere(frustrumPoints[i], 0.1f);
+	float GetMinimumCollisionDistanceFromTarget(Vector3[] nearPlaneFrustrumPoints)
+	{
+		float distance = -1.0f;
+		for (int i = 0; i < nearPlaneFrustrumPoints.Length; ++i)
+		{
+			Vector3 targetToFrustrumPoint = nearPlaneFrustrumPoints[i] - FollowedObject.transform.position;
+			float maxDistance = targetToFrustrumPoint.magnitude;
+			RaycastHit raycastHitInfo;
+
+			bool hit = Physics.Raycast(FollowedObject.transform.position,
+				targetToFrustrumPoint.normalized,
+				out raycastHitInfo, maxDistance);
+
+			if (hit)
+			{
+				if (raycastHitInfo.collider.gameObject != FollowedObject)
+				{
+					if (distance == -1.0f)
+					{
+						distance = raycastHitInfo.distance;
+					}
+					else
+					{
+						if (raycastHitInfo.distance < distance)
+						{
+							distance = raycastHitInfo.distance;
+						}
+					}
+				}
+			}
 		}
 
-		Matrix4x4 cameraToWorld = Camera.main.transform.localToWorldMatrix;
-		Matrix4x4 projection = Camera.main.projectionMatrix;
-		Vector4 axisX = cameraToWorld.GetColumn(0);
-		Vector4 axisY = cameraToWorld.GetColumn(1);
-		Vector4 axisZ = cameraToWorld.GetColumn(2);
+		return distance;
+	}
 
-		Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), axisX);
-		Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), axisY);
-		Gizmos.DrawLine(new Vector3(0.0f, 0.0f, 0.0f), axisZ);
-    }
-
-	Vector3[] ExtractFrustrumNearPlanePoints()
+	Vector3[] ExtractFrustrumNearPlanePoints(CameraDesc camera)
 	{
-		Matrix4x4 cameraToWorld = Camera.main.transform.localToWorldMatrix;
-		Matrix4x4 projection = Camera.main.projectionMatrix;
-
-		float zn = Camera.main.nearClipPlane;
-		float fovRadians = Camera.main.fieldOfView * (Mathf.PI / 180.0f);
+		float zn = camera.nearClipPlane;
+		float fovRadians = camera.fieldOfView * (Mathf.PI / 180.0f);
 		float e = Mathf.Tan(fovRadians * 0.5f);
 
-		Vector4 axisX = cameraToWorld.GetColumn(0);
-		Vector4 axisY = cameraToWorld.GetColumn(1);
-		Vector4 axisZ = cameraToWorld.GetColumn(2);
+		Vector4 axisX = camera.view.GetColumn(0);
+		Vector4 axisY = camera.view.GetColumn(1);
+		Vector4 axisZ = camera.view.GetColumn(2);
 
 		Vector3 nearCenter = axisZ * zn;
 
 		float nearExtX = e * zn;
-		float nearExtY = nearExtX / Camera.main.aspect;
+		float nearExtY = nearExtX / camera.aspect;
 
-		Vector4 cameraPos = Camera.main.transform.position;
+		Vector4 cameraPos = camera.position;
 
 		Vector4 BL = cameraPos + axisX * -nearExtX + axisY * -nearExtY + axisZ * zn;
 		Vector4 BR = cameraPos + axisX *  nearExtX + axisY * -nearExtY + axisZ * zn;
