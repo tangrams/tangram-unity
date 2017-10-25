3ecb28a84a3aa6b001bb76a182455fe90bee5824
Varun Talwar <tallytalwar@gmail.com>
2017-10-25 13:20:28 -0700

    Better controller movement with respect to camera direction
    - updated some constants and removed some control logic

--- Assets/Game/Chracter-Control/Scripts/ChracterController.cs
+++ Assets/Game/Chracter-Control/Scripts/ChracterController.cs
@@ -4,48 +4,37 @@ using UnityEngine;
 
 public class ChracterController : MonoBehaviour {
 
-    public float speed = 1.0f;
-    public float forceMult = 1.0f;
-    public float turnSpeed = 1.0f;
+    public float forceMult = 30.0f;
+    public float forceMultDirection = 60.0f;
 
     private Rigidbody rb;
-    private float powerInput;
-    private float turnInput;
 
     void Start() 
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void update() 
-    {
-        powerInput = Input.GetAxis ("Vertical");
-        turnInput = Input.GetAxis ("Horizontal");
-    }
-
     void FixedUpdate() 
     {
-        
-        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
-            float moveH = Input.GetAxis("Horizontal");
-            float moveV = Input.GetAxis("Vertical");
+            if (SystemInfo.deviceType == DeviceType.Desktop)
+            {
+                float moveH = Input.GetAxis("Horizontal");
+                float moveV = Input.GetAxis("Vertical");
 
-            Vector3 move = new Vector3(moveH, 0.0f, moveV);
-            rb.AddForce(move * forceMult);
+                Vector3 move = new Vector3(moveH * forceMultDirection, 0.0f, moveV * forceMult);
+                move = Camera.main.transform.TransformDirection(move);
+                rb.AddForce(move);
+            }
+            else
+            {
+                // TODO: Test
+                float moveH = Input.acceleration.x;
+                float moveV = -Input.acceleration.z;
+
+                Vector3 move = new Vector3(moveH * forceMultDirection, 0.0f, moveV * forceMult);
+                rb.AddForce(move);
+            }
         }
-        else
-        {
-            // TODO: Test
-            float moveH = Input.acceleration.x;
-            float moveV = -Input.acceleration.z;
-
-            Vector3 move = new Vector3(moveH, 0.0f, moveV);
-            rb.AddForce(move * forceMult);
-        }
-
-        rb.AddRelativeForce(0f, 0f, powerInput * speed);
-        rb.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
-
     }
 }
