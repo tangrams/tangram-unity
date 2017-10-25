d9458d355ba65d79fbcec0dcea0623ad00f04344
Varun Talwar <tallytalwar@gmail.com>
2017-10-25 14:17:55 -0700

    More character control
    - restrict y force to 0.0
    - have a maxVelocity which our sphere can reach

--- Assets/Game/Chracter-Control/Scripts/ChracterController.cs
+++ Assets/Game/Chracter-Control/Scripts/ChracterController.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class ChracterController : MonoBehaviour {
 
     public float forceMult = 30.0f;
-    public float forceMultDirection = 60.0f;
+    public float maxVelocity = 50.0f;
 
     private Rigidbody rb;
+    private float maxSqVelocity;
 
-    void Start() 
+    void Awake() // Recommended to use Awake instead of Start here.
     {
         rb = GetComponent<Rigidbody>();
+        maxSqVelocity = maxVelocity * maxVelocity;
+
     }
 
     void FixedUpdate() 
     {
+        if (rb.velocity.sqrMagnitude > maxVelocity)
+        {
+            rb.velocity = rb.velocity.normalized * maxVelocity;
+        }
         {
             if (SystemInfo.deviceType == DeviceType.Desktop)
             {
                 float moveH = Input.GetAxis("Horizontal");
                 float moveV = Input.GetAxis("Vertical");
 
-                Vector3 move = new Vector3(moveH * forceMultDirection, 0.0f, moveV * forceMult);
+                Vector3 move = new Vector3(moveH, 0.0f, moveV);
                 move = Camera.main.transform.TransformDirection(move);
-                rb.AddForce(move);
+                move.y = 0.0f;
+                rb.AddForce(move * forceMult);
             }
             else
             {
@@ -32,8 +40,9 @@ public class ChracterController : MonoBehaviour {
                 float moveH = Input.acceleration.x;
                 float moveV = -Input.acceleration.z;
 
-                Vector3 move = new Vector3(moveH * forceMultDirection, 0.0f, moveV * forceMult);
-                rb.AddForce(move);
+                Vector3 move = new Vector3(moveH, 0.0f, moveV);
+                move = Camera.main.transform.TransformDirection(move);
+                rb.AddForce(move * maxVelocity);
             }
         }
     }
