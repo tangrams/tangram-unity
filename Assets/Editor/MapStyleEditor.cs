44d8fbdd67c4c27622630584b84f674509c54f20
Matt Blair <blair1618@gmail.com>
2017-11-09 18:10:10 -0800

    Add new basic Map Style editor with TreeView of layers

---
+++ Assets/Editor/MapStyleEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace Mapzen.Unity.Editor
+{
+    [CustomEditor(typeof(MapStyle))]
+    public class MapStyleEditor : UnityEditor.Editor
+    {
+        [SerializeField]
+        TreeViewState layerTreeViewState;
+
+        FeatureLayerTreeView layerTreeView;
+
+        void OnEnable()
+        {
+            // Check whether we already had a serialized tree view state.
+            if (layerTreeViewState == null)
+            {
+                layerTreeViewState = new TreeViewState();
+            }
+            layerTreeView = new FeatureLayerTreeView(layerTreeViewState);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            MapStyle mapStyle = serializedObject.targetObject as MapStyle;
+            if (mapStyle == null)
+            {
+                // ????
+                return;
+            }
+
+            layerTreeView.Layers = mapStyle.Layers;
+            layerTreeView.Reload();
+            layerTreeView.OnGUI(GUILayoutUtility.GetRect(0, 500, 0, 150));
+
+            var selectedLayers = layerTreeView.GetSelection();
+
+            var layerArrayProperty = serializedObject.FindProperty("Layers");
+
+            var deletingLayers = false;
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Add Layer"))
+            {
+                mapStyle.Layers.Add(new FeatureLayer("untitled"));
+            }
+            if (GUILayout.Button("Remove Selected"))
+            {
+                foreach (var index in selectedLayers.OrderByDescending(i => i))
+                {
+                    layerArrayProperty.DeleteArrayElementAtIndex(index);
+                }
+                deletingLayers = true;
+            }
+            GUILayout.EndHorizontal();
+
+            if (!deletingLayers && selectedLayers.Count == 1)
+            {
+                var index = selectedLayers[0];
+
+                var layerProperty = layerArrayProperty.GetArrayElementAtIndex(index);
+                EditorGUILayout.PropertyField(layerProperty, true, null);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
