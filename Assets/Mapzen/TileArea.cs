14a31d6128ecf1d72762360d8a838c4c9b299fd8
Matt Blair <blair1618@gmail.com>
2017-11-26 20:35:23 -0800

    Split TileArea into a separate file from TileBounds

---
+++ Assets/Mapzen/TileArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mapzen
+{
+    [System.Serializable]
+    public struct TileArea
+    {
+        public LngLat min;
+        public LngLat max;
+        public int zoom;
+
+        public TileArea(LngLat min, LngLat max, int zoom)
+        {
+            this.min = min;
+            this.max = max;
+            this.zoom = zoom;
+        }
+    }
+}
--- Assets/Mapzen/TileBounds.cs
+++ Assets/Mapzen/TileBounds.cs
@@ -3,21 +3,6 @@ using System.Collections.Generic;
 
 namespace Mapzen
 {
-    [System.Serializable]
-    public struct TileArea
-    {
-        public LngLat min;
-        public LngLat max;
-        public int zoom;
-
-        public TileArea(LngLat min, LngLat max, int zoom)
-        {
-            this.min = min;
-            this.max = max;
-            this.zoom = zoom;
-        }
-    }
-
     public class TileBounds
     {
         public readonly TileAddress min;
