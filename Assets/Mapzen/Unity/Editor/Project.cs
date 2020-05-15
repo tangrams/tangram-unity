299e40c5e07b3f9a940ed9e71f468999463d4c73
Matt Blair <blair1618@gmail.com>
2020-05-15 03:40:29 -0400

    Use a build method to export the package

---
+++ Assets/Mapzen/Unity/Editor/Project.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Mapzen.Unity.Editor
+{
+    public class Project
+    {
+        public static void ExportPackage()
+        {
+            AssetDatabase.ExportPackage("Assets", "tangram-unity.unitypackage", ExportPackageOptions.Recurse);
+        }
+    }
+}
